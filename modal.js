4646301ca1d33e8eee1eb31f43cc6990ea8dd29c
istarlet <istarlet@gmail.com>
2022-05-29 00:26:59 -0400

    Add comment to modal.js

--- modal.js
+++ modal.js
@@ -1,3 +1,5 @@
+// Based on Kevin Powell's Modals the Easy Way
+
 const modal = document.querySelector("#modal");
 const openModal = document.querySelector(".open-button");
 const closeModal = document.querySelector(".close-button");
